9eb6b9a65ef11a39009719347938fb0a01ba043d
ZaoLahma <thyberg.jan@gmail.com>
2023-08-25 22:35:50 +0200

    Implement busy wait frame limiter thingy

--- playground/core/game/game.py
+++ playground/core/game/game.py
@@ -1,17 +1,32 @@
 from core.event.event_quit import EventQuit
+import time
 
 # The thing that makes all other things do things
 class Game():
-    def __init__(self):
+    def __init__(self, targetFPS, useBusyLoop = True):
+        self.m_targetFPS = targetFPS
+        self.m_useBusyLoop = useBusyLoop
+        self.m_targetFrameTime = 1.0 / self.m_targetFPS
         self.m_runHooks = []
         self.m_running = False
 
     # The game loop. TODO: Make this run at a predictable rate
     def run(self):
         self.m_running = True
+        prevTime = time.perf_counter()
+        currentTime = prevTime
         while self.m_running:
+            prevTime = currentTime
+            currentTime = time.perf_counter()
+            deltaTime = currentTime - prevTime
             for runHook in self.m_runHooks:
                 runHook()
+            if self.m_useBusyLoop:
+                while time.perf_counter() < currentTime + self.m_targetFrameTime:
+                    pass
+            else:
+                raise NotImplementedError
+
 
     # Ugly-ish hack-ish way of hooking in the Game class to the event handling parts
     def registerEventDispatcher(self, eventDispatcher):
--- playground/main.py
+++ playground/main.py
@@ -3,8 +3,8 @@ from core.game.game import Game
 from core.event.event_dispatcher import EventDispatcher
 
 if __name__ == "__main__":
-    print("Hello world!")
-    game = Game()
+    targetFps = 60
+    game = Game(targetFps)
     eventDispatcher = EventDispatcher(game)
     game.registerEventDispatcher(eventDispatcher)
     window = Window(game, eventDispatcher)
