a5cb1f4a3ca9f61d20228265b09d2ff00b2eb8d7
ZaoLahma <thyberg.jan@gmail.com>
2023-08-26 23:17:09 +0200

    Improve(?) the game loop thingy

--- playground/core/game/game.py
+++ playground/core/game/game.py
@@ -10,22 +10,34 @@ class Game():
         self.m_runHooks = []
         self.m_running = False
 
-    # The game loop. TODO: Make this run at a predictable rate
+    # The game loop. Can either use busy loop (hog the CPU until it's time to do stuff) 
+    # or sleep (play nice and share the toys)
+    # High FPS applications probably want to use the busy loop
+    # The end goal is to make sure to keep the frame times consistent
     def run(self):
         self.m_running = True
         prevTime = time.perf_counter()
         currentTime = prevTime
         while self.m_running:
-            prevTime = currentTime
-            currentTime = time.perf_counter()
-            deltaTime = currentTime - prevTime
+            prevTime = time.perf_counter()
+
             for runHook in self.m_runHooks:
                 runHook()
+
+            currentTime = time.perf_counter()
+
             if self.m_useBusyLoop:
                 while time.perf_counter() < currentTime + self.m_targetFrameTime:
                     pass
             else:
-                raise NotImplementedError
+                deltaTimeS = (currentTime - prevTime)
+                toSleep = self.m_targetFrameTime - deltaTimeS
+                if toSleep > 0:
+                    time.sleep(toSleep)
+            totalFrameTime = (time.perf_counter() - currentTime) * 1000
+            print("Total frame time: " + "{:.4f}".format(totalFrameTime) + "ms")
+            fps = 1000 / totalFrameTime
+            print("fps: " + "{:.4f}".format(fps))
 
 
     # Ugly-ish hack-ish way of hooking in the Game class to the event handling parts
--- playground/main.py
+++ playground/main.py
@@ -3,10 +3,10 @@ from core.game.game import Game
 from core.event.event_dispatcher import EventDispatcher
 
 if __name__ == "__main__":
-    targetFps = 60
-    game = Game(targetFps)
+    game = Game(targetFPS = 60, useBusyLoop = False)
     eventDispatcher = EventDispatcher(game)
     game.registerEventDispatcher(eventDispatcher)
     window = Window(game, eventDispatcher)
 
+    # Fire in the hole
     game.run()
